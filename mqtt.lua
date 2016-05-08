5587cd5af40cfd900f212c79b684160742d08187
pradeesi <pradeep.si@gmail.com>
2016-05-08 10:16:05 +0530

    Update mqtt.lua

--- mqtt.lua
+++ mqtt.lua
@@ -18,7 +18,7 @@ MQTT_PUBLISH_TOPIC_QoS = 0
 MQTT_PUBLISH_TOPIC_RETAIN = 0
 MQTT_PUBLISH_MESSAGE = "Hello MQTT"
 
-MQTT_SUBSCRIBE_TOPIC =  "my_topic"
+MQTT_SUBSCRIBE_TOPIC = "my_topic"
 MQTT_SUBSCRIBE_TOPIC_QoS = 0
 
 MQTT_CLIENT_ID = "client1"
@@ -27,48 +27,40 @@ MQTT_CLIENT_PASSWORD = "password"
 MQTT_CLIENT_KEEPALIVE_TIME = 120
 ------------------------------------------
 
-
-
 --- Initiate MQTT Client ---
-mqtt = mqtt.Client(MQTT_CLIENT_ID, MQTT_CLIENT_KEEPALIVE_TIME, MQTT_CLIENT_USER, MQTT_CLIENT_PASSWORD)
-
+MQTT_Client = mqtt.Client(MQTT_CLIENT_ID, MQTT_CLIENT_KEEPALIVE_TIME, MQTT_CLIENT_USER, MQTT_CLIENT_PASSWORD)
 
 --- On Connect Event ---
-mqtt:on("connect", function(con) 
-     print ("connected") 
+MQTT_Client:on("connect", function(con) 
+ print ("connected") 
 end)
-
 
 --- Offline Event ---
-mqtt:on("offline", function(con) 
-     print ("offline") 
+MQTT_Client:on("offline", function(con) 
+ print ("offline") 
 end)
-
 
 -- On Message Recieve Event ---
 -- This Function will be invoked everytime Broker sends message to subscribed Topic ---
-mqtt:on("message", function(conn, topic, data)
-  print("MQTT Message Received...")
-  print("Topic: " .. topic)
-  if data ~= nil then
-    print("Message: " .. data)
-  end
+MQTT_Client:on("message", function(conn, topic, data)
+ print("MQTT Message Received...")
+ print("Topic: " .. topic)
+ if data ~= nil then
+ print("Message: " .. data)
+ end
 end)
-
 
 --- Connect to MQTT Broker ----
-mqtt:connect(MQTT_BROKER, MQTT_BROKER_PORT, MQTT_BROKER_SECURE, function(conn) 
-  print("connected")
+MQTT_Client:connect(MQTT_BROKER, MQTT_BROKER_PORT, MQTT_BROKER_SECURE, function(conn) 
+ print("connected")
 end)
-
 
 -- Subscribe to MQTT Topic ---
-mqtt:subscribe(MQTT_SUBSCRIBE_TOPIC,MQTT_SUBSCRIBE_TOPIC_QoS, function(conn) 
+MQTT_Client:subscribe(MQTT_SUBSCRIBE_TOPIC,MQTT_SUBSCRIBE_TOPIC_QoS, function(conn) 
 end)
 
-  
 -- Publish to MQTT Topic ---
 -- Use this Function to publish message on MQTT Topic ---
-mqtt:publish(MQTT_PUBLISH_TOPIC, MQTT_PUBLISH_MESSAGE, MQTT_PUBLISH_TOPIC_QoS, MQTT_PUBLISH_TOPIC_RETAIN, function(conn) 
-  print("Message Published on MQTT Topic...") 
+MQTT_Client:publish(MQTT_PUBLISH_TOPIC, MQTT_PUBLISH_MESSAGE, MQTT_PUBLISH_TOPIC_QoS, MQTT_PUBLISH_TOPIC_RETAIN, function(conn) 
+ print("Message Published on MQTT Topic...") 
 end)
